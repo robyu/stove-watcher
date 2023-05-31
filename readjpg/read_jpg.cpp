6e75945daa2f38e263bd05389c3957df20a84ba8
robert yu <rob@signalessence.com>
2023-05-30 21:06:45 -0700

    it lives!

--- readjpg/read_jpg.cpp
+++ readjpg/read_jpg.cpp
@@ -2,9 +2,10 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <iomanip>
 #include <opencv2/opencv.hpp>
 
-std::vector<int> readJPG(const std::string& filename, int& width, int& height) {
+std::vector<std::uint32_t> readJPG(const std::string& filename, int& width, int& height) {
     cv::Mat image = cv::imread(filename, cv::IMREAD_COLOR);
 
     if (image.empty()) {
@@ -14,8 +15,8 @@ std::vector<int> readJPG(const std::string& filename, int& width, int& height) {
     width = image.cols;
     height = image.rows;
 
-    std::vector<int> pixelValues;
-    pixelValues.reserve(width * height);
+    std::vector<std::uint32_t> rgbValues;
+    rgbValues.reserve(width * height);
 
     for (int y = 0; y < height; ++y) {
         for (int x = 0; x < width; ++x) {
@@ -24,53 +25,47 @@ std::vector<int> readJPG(const std::string& filename, int& width, int& height) {
             int green = pixel[1];
             int blue = pixel[0];
 
-            int rgbValue = (red << 16) | (green << 8) | blue;
-            pixelValues.push_back(rgbValue);
+            std::uint32_t rgbValue = (red << 16) | (green << 8) | blue;
+            rgbValues.push_back(rgbValue);
         }
     }
 
-    return pixelValues;
+    return rgbValues;
 }
 
-// Function to convert RGB values to hex format
-std::vector<std::string> convertToHex(const std::vector<int>& rgb, int width, int height) {
-    std::vector<std::string> hexValues(width * height);
 
-    for (int i = 0; i < width * height; ++i) {
-        int r = (rgb[i] >> 16) & 0xFF;
-        int g = (rgb[i] >> 8) & 0xFF;
-        int b = rgb[i] & 0xFF;
 
-        // Convert RGB values to hex format: 0xRRGGBB
-        std::string hexValue = "0x";
-        hexValue += ((r < 16) ? "0" : "") + std::to_string(r);
-        hexValue += ((g < 16) ? "0" : "") + std::to_string(g);
-        hexValue += ((b < 16) ? "0" : "") + std::to_string(b);
+void prettyPrintHex(const std::vector<std::uint32_t>& values,
+                    int numValuesToPrint,
+                    int numValuesPerLine) {
 
-        hexValues[i] = hexValue;
-    }
-
-    return hexValues;
-}
-
-// Test case for readJPG and convertToHex functions
-void testReadJPG() {
-    std::string filename = "example.jpg";
-    int width, height;
-    std::vector<int> pixels = readJPG(filename, width, height);
-
-    std::vector<std::string> hexValues = convertToHex(pixels, width, height);
-
-    // Print the hex values for verification
-    for (const auto& hexValue : hexValues) {
-        std::cout << hexValue << std::endl;
-    }
+    for (int i = 0; i < std::min(numValuesToPrint, static_cast<int>(values.size())); ++i) {
+        std::cout << "0x" << std::setfill('0') << std::setw(6) << std::hex << values[i] << " ";
+        if ((i + 1) % numValuesPerLine == 0) {
+            std::cout << std::endl;
+        }
+    }				
 }
 
 // Entry point of the program
-int main() {
-    // Run the test case
-    testReadJPG();
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <input_file>" << std::endl;
+        return 1;
+    }
+
+    std::string inputFilename = argv[1];
+
+    // Call the readJPG function with the input filename
+    int width;
+    int height;
+    std::vector<std::uint32_t> rgbValues = readJPG(inputFilename, width, height);
+
+    std::cout << "Read file " << inputFilename << " ("<< width << ", " << height << ")" << std::endl;
+    
+    
+    prettyPrintHex(rgbValues, 10, 5);
+
 
     return 0;
 }
